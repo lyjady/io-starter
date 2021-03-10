77e0384cc88f565da8f5e3297ed80c9d649b3028
991296755@qq.com <lyj112233>
2021-03-10 14:34:08 +0800

    nio server

---
+++ src/main/java/org/augustus/bio/Jmap.java
@@ -0,0 +1,19 @@
+package org.augustus.bio;
+
+import java.util.Scanner;
+
+/**
+ * @author: linyongjin
+ * @create: 2021-03-07 18:18:21
+ */
+public class Jmap {
+
+    public static void main(String[] args) {
+        byte[] bytes = new byte[2048];
+        for (int i = 0; i < 2048; i++) {
+            bytes[i] = 1;
+        }
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+}
---
+++ src/main/java/org/augustus/nio/ChatServer.java
@@ -0,0 +1,105 @@
+package org.augustus.nio;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+
+/**
+ * @author: linyongjin
+ * @create: 2021-03-09 20:41:44
+ */
+public class ChatServer {
+
+    private int port;
+
+    private final int CAPACITY = 1024;
+
+    private final String QUIT = "quit";
+
+    private ServerSocketChannel serverSocketChannel;
+
+    private Selector selector;
+
+    private final ByteBuffer readBuffer = ByteBuffer.allocate(CAPACITY);
+
+    private final ByteBuffer writeBuffer = ByteBuffer.allocate(CAPACITY);
+
+    public ChatServer() {
+        this(8080);
+    }
+
+    public ChatServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        try {
+            // 构建ServerSocketChannel并绑定端口
+            this.serverSocketChannel = ServerSocketChannel.open();
+            this.serverSocketChannel.configureBlocking(false);
+            this.serverSocketChannel.socket().bind(new InetSocketAddress(this.port));
+            // 构建Selector
+            this.selector = Selector.open();
+            // 向Selector注册ServerSocketChannel并监听Accept事件
+            this.serverSocketChannel.register(this.selector, SelectionKey.OP_ACCEPT);
+            while (true) {
+                this.selector.select();
+                Set<SelectionKey> selectionKeys = this.selector.selectedKeys();
+                for (SelectionKey selectionKey : selectionKeys) {
+                    handles(selectionKey);
+                }
+                selectionKeys.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handles(SelectionKey selectionKey) throws IOException {
+        if (selectionKey.isAcceptable()) {
+            SocketChannel socketChannel = this.serverSocketChannel.accept();
+            socketChannel.configureBlocking(false);
+            socketChannel.register(this.selector, SelectionKey.OP_READ);
+            System.out.printf("客户端%s已经连接到服务器", socketChannel.socket().getPort());
+            System.out.println();
+        } else if (selectionKey.isReadable()) {
+            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+            String message = this.readMessageFormSocketChannel(socketChannel);
+            if (message.isEmpty()) {
+                selectionKey.cancel();
+                this.selector.wakeup();
+            } else {
+                System.out.printf("客户端%s的消息: %s", socketChannel.socket().getPort(), message);
+                System.out.println();
+                if (QUIT.equalsIgnoreCase(message)) {
+                    selectionKey.cancel();
+                    this.selector.wakeup();
+                    System.out.printf("客户端%s已断开", socketChannel.socket().getPort());
+                    System.out.println();
+                }
+                this.forwardMessage(socketChannel, message);
+            }
+        }
+    }
+
+    private String readMessageFormSocketChannel(SocketChannel socketChannel) throws IOException {
+        this.readBuffer.clear();
+        while (socketChannel.read(readBuffer) != -1) {}
+        return String.valueOf(StandardCharsets.UTF_8.decode(readBuffer));
+    }
+
+    private void forwardMessage(SocketChannel socketChannel, String message) {}
+
+    private void close(Closeable... closeables) throws IOException {
+        for (Closeable closeable : closeables) {
+            closeable.close();
+        }
+    }
+}
